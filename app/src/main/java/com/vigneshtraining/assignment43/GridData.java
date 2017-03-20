a2648f437abbc7d4d2b2511188ad74e3500bf7fe
VigneshMadhavan <vigneshmadhavan@yahoo.co.in>
2017-03-20 23:01:45 +0530

    Initial commit

---
+++ app/src/main/java/com/vigneshtraining/assignment43/GridData.java
@@ -0,0 +1,21 @@
+package com.vigneshtraining.assignment43;
+
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by vimadhavan on 3/20/2017.
+ */
+
+public class GridData {
+    public Integer getImage() {
+        return image;
+    }
+
+    public void setImage(Integer image) {
+        this.image = image;
+    }
+
+    private Integer image;
+
+}
---
+++ app/src/main/java/com/vigneshtraining/assignment43/Loader.java
@@ -0,0 +1,22 @@
+package com.vigneshtraining.assignment43;
+
+import android.widget.GridView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by vimadhavan on 3/20/2017.
+ */
+
+public class Loader {
+
+    private GridItem adapter;
+    private GridView gridView;
+    private ArrayList<GridData> gridDatas;
+
+   
+
+
+
+
+}
